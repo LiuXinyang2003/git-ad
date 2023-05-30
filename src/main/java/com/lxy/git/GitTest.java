df5c503c8584b85cad40f2676227244388f21f84
LiuXinyang2003 <771093658@qq.com>
2023-05-30 10:47:59 +0800

    second commit

---
+++ src/main/java/com/lxy/git/GitTest.java
@@ -0,0 +1,21 @@
+package com.lxy.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.test.git
+ * Description:
+ *
+ * @Author: 刘鑫洋
+ * @Create: 2023/5/29 -15:13
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master git!");
+        System.out.println("hot-fix git!");
+    }
+}
